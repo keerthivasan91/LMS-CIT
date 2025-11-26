6790edcb353170970feb4bd4a9f192b6b286b748
keerthivasan91 <keerthivasan1617@gmail.com>
2025-11-26 22:04:36 +0530

    feat: Integrate React Query for data fetching with QueryClientProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 
+// Create a single React Query client instance
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
