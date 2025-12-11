3b0b6ed5ff8b3f3f81f4a3c6a6395bc7283aa75c
keerthivasan91 <keerthivasan1617@gmail.com>
2025-12-11 14:52:01 +0530

    fix: add domain attribute for cookies in production environment

--- server/app.js
+++ server/app.js
@@ -53,6 +53,7 @@ app.use(
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite : process.env.NODE_ENV === 'production' ? 'none' : 'lax' , // same site means strict else none
+      domain: process.env.NODE_ENV === 'production' ? '.up.railway.app' : 'localhost',
       maxAge: (1000 * 60 * 30), // 30 minutes
     }
   })
