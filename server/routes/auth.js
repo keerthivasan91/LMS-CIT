46298926f3dc73e6e7d7cd2c5790f08dcccc7ce5
keerthivasan91 <keerthivasan1617@gmail.com>
2025-12-16 00:33:09 +0530

    fix: refactor login rate limiter to use middleware for improved readability

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const rateLimit = require("express-rate-limit");
+const loginLimiter  = require('../middleware/rateLimit');
 
 const { login, me, logout } = require('../controllers/authController');
 const sessionAuth = require('../middleware/authMiddleware');
 
-// Rate limiter only for login
-const loginLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 5,
-    message: "Too many login attempts. Try again later."
-});
 
 // Public Login
 router.post('/login', loginLimiter, login);
