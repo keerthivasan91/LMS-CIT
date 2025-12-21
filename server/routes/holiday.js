761a891658bdefd2126f9cb560d082fc5e313064
keerthivasan91 <keerthivasan1617@gmail.com>
2025-12-21 17:45:26 +0530

    fix: refactor holiday routes to include all CRUD operations for holiday management

--- server/routes/holiday.js
+++ server/routes/holiday.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getHolidays } = require('../controllers/holidaycontroller');
 const sessionAuth = require('../middleware/authMiddleware');
+const {
+  getHolidays,
+  addHoliday,
+  updateHoliday,
+  deleteHoliday
+} = require("../controllers/holidaycontroller");
 
 router.get('/',sessionAuth(), getHolidays);
+
+// Admin
+router.post("/", sessionAuth(["admin"]), addHoliday);
+router.put("/:id", sessionAuth(["admin"]), updateHoliday);
+router.delete("/:id", sessionAuth(["admin"]), deleteHoliday);
 
 module.exports = router;
